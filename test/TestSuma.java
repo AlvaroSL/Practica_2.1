8435a67df7e3c2e1486a12f48285d377133fcdc0
AlvaroSL <varosl94@gmail.com>
2016-10-26 18:51:25 +0200

    Primer commit

---
+++ test/TestSuma.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuma {
+
+	@Test
+	public void testSum2Num(){
+		assertEquals(6, sum(2,4));
+	}
+	
+	private int sum(int i, int j){
+		return 6;
+	}
+	
+	
+}
