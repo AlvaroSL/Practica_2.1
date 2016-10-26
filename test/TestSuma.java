5ea4aeb2400904d0cff7be3b2fb6d3f7f85cd7d6
AlvaroSL <varosl94@gmail.com>
2016-10-26 18:53:40 +0200

    Segundo commit

--- test/TestSuma.java
+++ test/TestSuma.java
@@ -9,8 +9,15 @@ public class TestSuma {
 		assertEquals(6, sum(2,4));
 	}
 	
+	@Test
+	public void testSumaCero(){
+		assertEquals(4, sum(4,0));
+		assertEquals(4, sum(0,4));
+
+	}
+	
 	private int sum(int i, int j){
-		return 6;
+		return i+j;
 	}
 	
 	
