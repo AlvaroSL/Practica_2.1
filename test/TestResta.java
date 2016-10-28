b81fe70b59c8dc01ad6321c0fa05f34b293e278a
AlvaroSL <varosl94@gmail.com>
2016-10-28 09:58:35 +0200

    Primera prueba

---
+++ test/TestResta.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Suma.Suma;
+
+public class TestResta {
+
+	@Test
+	public void testResta2Num(){
+		assertEquals(2, resta(5,3));
+	}
+	
+	
+	private int resta(int i, int j){
+		return 2;
+	}
+	
+	
+}
